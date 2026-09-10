64866f03e100856f352ce363519af8f0ede4b26d
Araame <aram171002@gmail.com>
2026-09-10 09:32:28 +0000

    Initialization

---
+++ src/main/java/com/habiterra/HabiTerraBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.habiterra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiTerraBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabiTerraBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/habiterra/HabiTerraBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habiterra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabiTerraBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
